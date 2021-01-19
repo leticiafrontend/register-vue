58ba5068c6ed315a07c0137584745aec0f08edab
leticiafrontend <leticia.alesilva@outlook.com>
2021-01-19 14:06:45 -0300

    :lipstick: - Creating header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
   <div id="app">
-    <p>Hello World</p>
+    <HeaderApp />
+    <main id="main"> 
+      <router-view/>
+    </main>
   </div>
 </template>
 
+<script>
+import HeaderApp from '@/components/HeaderApp.vue'
+
+export default {
+  components: {
+    HeaderApp
+  }
+}
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+
+*{
+  padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  font-family: 'Quicksand', sans-serif;
+}
+
+body{
+  background-color: #fbfbfb;
+}
+
+main{
+  margin: 0 auto;
+  max-width: 430px;
+  padding: 50px 20px 0;
+}
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <div class="header__container" id='nav'>
+      <img class="header__image" src="../assets/logo.png" alt="Logo Vue">
+      <nav>
+        <router-link class="link" to="/">Cadastro</router-link>
+        <router-link class="link" to="/users">Usuários</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderApp'
+}
+</script>
+
+<style scoped>
+
+header{
+  background-color: #F05E5C;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#nav{
+  max-width: 430px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10px;
+}
+
+#nav img{
+  width: 10%;
+}
+
+#nav .link{
+  text-decoration: none;
+  color: #ffff;
+  font-weight: 600;
+}
+
+#nav .link:last-child {
+  margin-left: 12px;
+}
+
+</style>
