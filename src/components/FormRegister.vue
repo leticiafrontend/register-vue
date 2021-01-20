06f04cfd035f4015463019c0317167a69ba35b1b
leticiafrontend <leticia.alesilva@outlook.com>
2021-01-20 08:30:27 -0300

    :construction: - Adjustments in validations

--- src/components/FormRegister.vue
+++ src/components/FormRegister.vue
@@ -1,67 +1,131 @@
 <template>
   <div class="register">
-    <form>
+    <form @submit.prevent="checkForm">
         <label for="name">Nome</label>
         <input @change="nameValidation" name="name" type="text" v-model="name" placeholder="Nome Completo">
+        <template v-if="errorName.length">
+          <p class="error" v-for="error in errorName" :key="error">{{ error }}</p>
+        </template>
         <label for="cpf">CPF</label>
         <input @change="cpfValidation" name="cpf" type="text" v-model="cpf" placeholder="000.000.000-00">
+        <template v-if="errorCpf.length">
+          <p class="error" v-for="error in errorCpf" :key="error">{{ error }}</p>
+        </template>
         <label for="phone">Telefone</label>
         <input @change="phoneValidation" name="phone" type="text" v-model="phone" placeholder="+55 11 99999-9999">
+        <template v-if="errorPhone.length">
+          <p class="error" v-for="error in errorPhone" :key="error">{{ error }}</p>
+        </template>
         <label for="email">Email</label>
         <input @change="emailValidation" name="email" type="text" v-model="email" placeholder="email@email.com">
-        <button @click.prevent="registerUser" type="submit">Cadastrar</button>
+        <template v-if="errorEmail.length">
+          <p class="error" v-for="error in errorEmail" :key="error">{{ error }}</p>
+        </template>
+        <button type="submit">Cadastrar</button>
       </form>
-      <template v-show="{success}">
-        <Notification />
-      </template>
   </div>
 </template>
 
 <script>
 
-import Notification from './Notification.vue'
-
 export default {
   name: 'FormRegister',
   components: {
-    Notification
   },
   data(){
     return{
-      sucess: false,
+      errorName: [],
+      errorCpf: [],
+      errorPhone: [],
+      errorEmail: [],
+      name: '',
+      cpf: '',
+      phone: '',
+      email: '',
+      userData: [],
     }
   },
   methods: {
     nameValidation(event){
       const input = event.target
       if(input.value.length < 3){
+        this.errorName = []
         input.classList.add('error')
+        this.errorName.push('Preencha o nome corretamente!')
       } else{
         input.classList.remove('error')
+        this.errorName = []
       }
     },
     cpfValidation(event){
       const input = event.target
       if(input.value.length < 11){
+        this.errorCpf = []
         input.classList.add('error')
+        this.errorCpf.push('Preencha o CPF corretamente!')
       } else{
         input.classList.remove('error')
+        this.errorCpf = []
       }
     },
     phoneValidation(event){
       const input = event.target
       if(input.value.length < 10){
+        this.errorPhone = []
         input.classList.add('error')
+        this.errorPhone.push('Preencha o telefone corretamente!')
       } else{
         input.classList.remove('error')
+        this.errorPhone = []
       }
     },
     emailValidation(event){
       const input = event.target
       if(input.value.length < 10){
+        this.errorEmail = []
         input.classList.add('error')
+        this.errorEmail.push('Preencha o email corretamente!')
       } else{
         input.classList.remove('error')
+        this.errorEmail = []
+      }
+    },
+    checkForm(event){
+      this.errorName = []
+      this.errorCpf = []
+      this.errorPhone = []
+      this.errorEmail = []
+
+      if(this.name < 3){
+        this.errorName.push('Preencha o nome corretamente!')
+        event.target[0].classList.add('error')
+      }
+
+      if(this.cpf < 11){
+        this.errorCpf.push('Preencha o CPF corretamente!')
+        event.target[1].classList.add('error')
+      }
+
+      if(this.phone < 10){
+        this.errorPhone.push('Preencha o telefone corretamente!')
+        event.target[2].classList.add('error')
+      }
+
+      if(this.email < 10){
+        this.errorEmail.push('Preencha o email corretamente!')
+        event.target[3].classList.add('error')
+      }
+
+      if(this.errorName.length === 0 && this.errorCpf.length === 0 && this.errorPhone.length === 0 && this.errorEmail.length === 0){
+
+        this.userData.push({name: this.name, cpf: this.cpf, phone: this.phone, email: this.email})
+
+        this.name = ''
+        this.cpf = ''
+        this.phone = ''
+        this.email = ''
+
+        console.log(this.userData)
       }
     }
   }
@@ -130,5 +194,11 @@ form input.error{
   background-color: #FDEFEE;
   border: 2px solid #C25151;
 }
+
+p.error{
+  color: #C25151;
+  font-weight: 600;
+}
+
 
 </style>
